320194b0b809cf6e4aa8e25167a010754b64cf2e
VIVEK9898 <102471576+VIVEK9898@users.noreply.github.com>
2022-05-30 09:37:17 +0530

    Create Write a Python program to create a new dictionary by extracting the mentioned keys from the below dictionary.py

---
+++ DICTIONARY/Write a Python program to create a new dictionary by extracting the mentioned keys from the below dictionary.py
@@ -0,0 +1,10 @@
+sampleDict = { 
+  "name": "Kelly",
+  "age":25, 
+  "salary": 8000, 
+  "city": "New york" }
+
+keys = ["name", "salary"]
+
+newDict = {k: sampleDict[k] for k in keys}
+print(newDict)
