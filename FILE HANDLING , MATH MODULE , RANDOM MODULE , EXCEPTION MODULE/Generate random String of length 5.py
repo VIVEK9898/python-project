a7c575ef75a0f18426c60feb4eac2884c045e5f7
VIVEK9898 <102471576+VIVEK9898@users.noreply.github.com>
2022-05-30 09:56:18 +0530

    Create Generate random String of length 5.py

---
+++ FILE HANDLING , MATH MODULE , RANDOM MODULE , EXCEPTION MODULE/Generate random String of length 5.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+def randomString(stringLength):
+    """Generate a random string of 5 charcters"""
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+print ("Random String is ", randomString(5) )
