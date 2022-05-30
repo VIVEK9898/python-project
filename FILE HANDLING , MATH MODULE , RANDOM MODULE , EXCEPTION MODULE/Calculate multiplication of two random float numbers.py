7deb667995e66ab946975a37fe5dfe4a52b668ba
VIVEK9898 <102471576+VIVEK9898@users.noreply.github.com>
2022-05-30 09:54:37 +0530

    Create Calculate multiplication of two random float numbers.py

---
+++ FILE HANDLING , MATH MODULE , RANDOM MODULE , EXCEPTION MODULE/Calculate multiplication of two random float numbers.py
@@ -0,0 +1,9 @@
+import random
+
+num1  = random.random()
+print("First Random float is ", num1)
+num2 = random.uniform(9.5, 99.5)
+print("Second Random float is ", num1)
+
+num3 = num1 * num2
+print("Multiplication is ", num3)
