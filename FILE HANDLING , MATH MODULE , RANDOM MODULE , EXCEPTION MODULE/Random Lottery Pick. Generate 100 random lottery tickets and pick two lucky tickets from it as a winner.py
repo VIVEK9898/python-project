b0fac887ee6d0d06d77d71f98e245e0b636f0ca1
VIVEK9898 <102471576+VIVEK9898@users.noreply.github.com>
2022-05-30 10:01:18 +0530

    Create Random Lottery Pick. Generate 100 random lottery tickets and pick two lucky tickets from it as a winner.py

---
+++ FILE HANDLING , MATH MODULE , RANDOM MODULE , EXCEPTION MODULE/Random Lottery Pick. Generate 100 random lottery tickets and pick two lucky tickets from it as a winner.py
@@ -0,0 +1,11 @@
+import random
+
+lottery_tickets_list = []
+print("creating 100 random lottery tickets")
+# to get 100 ticket
+for i in range(100):
+    # ticket number must be 10 digit (1000000000, 9999999999)
+    lottery_tickets_list.append(random.randrange(1000000000, 9999999999))
+# pick 2 luck tickets
+winners = random.sample(lottery_tickets_list, 2)
+print("Lucky 2 lottery tickets are", winners)
